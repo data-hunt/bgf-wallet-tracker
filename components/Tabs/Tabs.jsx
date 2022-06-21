4acc761b15f68e91915e463e1101c786ecc47cf0
data-hunt <tannerhunt05@gmail.com>
2022-06-21 07:50:46 -0600

    added draft styling for tabs comp.

--- components/Tabs/Tabs.jsx
+++ components/Tabs/Tabs.jsx
@@ -1,37 +1,47 @@
-import { style } from "@mui/system";
 import React from "react";
 import styles from "./Tabs.module.css";
 import Link from "next/link";
-import Router from "next/router";
+import { withRouter } from "next/router";
+import Wrapper from "../Layout/Wrapper";
+import Container from "../Layout/Container";
 
-function Tabs() {
+function Tabs({ router }) {
 	const {
 		query: { tab },
-	} = Router;
+	} = router;
 
 	const isTabOne = tab === "1" || tab == null;
 	const isTabTwo = tab === "2";
 
 	return (
 		<div className={styles.tabContainer}>
-			<div className={styles.tabHead}>
-				<div className={styles.tab}>
-					<Link href={{ pathname: "/", query: { tab: "1" } }}>
-						<a>Dashboard</a>
-					</Link>
-				</div>
-				<div className={styles.tab}>
-					<Link href={{ pathname: "/", query: { tab: "2" } }}>
-						<a>Transactions</a>
-					</Link>
-				</div>
-				<div className={styles.tabBody}>
-					{isTabOne && <>Tab one content</>}
-					{isTabTwo && <>Tab two content</>}
-				</div>
-			</div>
+			<Wrapper className={styles.wrapper}>
+				<Container
+					className={styles.content}
+					alignItems='center'
+					justifyContent='space-between'
+				>
+					<div className={styles.tabHead}>
+						<div className={styles.tab}>
+							<Link href={{ pathname: "/", query: { tab: "1" } }}>
+								<a>Dashboard</a>
+							</Link>
+						</div>
+						<div className={styles.tab}>
+							<Link href={{ pathname: "/", query: { tab: "2" } }}>
+								<a>Transactions</a>
+							</Link>
+						</div>
+					</div>
+
+					<div className={styles.tabBody}>
+						{isTabOne && <>Tab one content</>}
+						{isTabTwo && <>Tab two content</>}
+					</div>
+				</Container>
+			</Wrapper>
 		</div>
 	);
 }
 
-export default Tabs;
+export default withRouter(Tabs);
